8d31321a5ac55ee707ebca616b1107195795d73c
MagnificentPako <my@procrastination.life>
2018-11-16 20:43:43 +0100

    initial commit

---
+++ default.nix
@@ -0,0 +1,20 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "emocgi";
+  src = ./src;
+
+  buildInputs = [ (import "/etc/nixos/pkgs/emojicode")
+                  (import "/home/paul/doc/dev/Cpp/emojicode-fcgi")
+                  fcgi
+                ];
+
+  buildPhase = ''
+    emojicodec main.emojic -S $EMOJICODE_PATH
+  '';
+
+  installPhase = ''
+    mkdir $out -p
+    cp main $out
+  '';
+
+}
